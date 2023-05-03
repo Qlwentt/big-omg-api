aecca46c5cee2dbc37bb36082dade6cf4c90cc23
Qlwentt <quai.wentt@gmail.com>
2023-05-02 23:04:23 -0400

    add mvp for big-omg backend

---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+
+from flask import Flask, Response, jsonify, request
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+
+from src.services.openai import OpenAI
+
+
+load_dotenv('.env')
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+
+@app.route("/")
+def hello_world():
+    return jsonify("Hello, World!")
+
+@app.post('/api/get-big-o')
+@cross_origin(supports_credentials=True)
+def get_big_o():
+    req = request.get_json()
+    if 'code' not in req:
+        return Response("Missing required parameter 'code'", status=422)
+    code = req['code']
+    openai_client = OpenAI()
+    big_o_data = openai_client.get_big_o(code)
+    response = json.dumps({"data": big_o_data})
+    return Response(response, status=200, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/services/openai.py
@@ -0,0 +1,29 @@
+import openai
+import os
+import re
+
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+class OpenAI:
+    def get_big_o(self, code):
+        messages = [
+            {"role": "system", "content": "You are an AI assistant that helps users analyze the time complexity of code snippets."},
+            {"role": "user", "content": f"I have the following piece of code:\n\n{code}\n\nWhat is the time complexity of this code and explain your reasoning?"},
+        ]
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            max_tokens=2048,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+
+        explanation = response['choices'][0]['message']['content'].strip()
+        big_o_pattern = r'O\([\w]+\)'
+        match = re.search(big_o_pattern, explanation)
+        time_complexity = match.group(0) if match else "Unknown"
+
+        return {"big_o": time_complexity, "explanation": explanation}
